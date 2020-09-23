e8d0d8d4866e176ec3e05e37a0889687ef53316b
trkohler <troyalekson@gmail.com>
2020-09-23 12:11:28 +0300

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,59 @@
-require(`dotenv`).config({
-  path: `.env`,
-})
-
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
-
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Трой Кёхлер`,
+    // Default title of the page
+    siteTitleAlt: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://minimal-blog.lekoarts.de`,
+    // Used for SEO
+    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `ru`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@lekoarts_de`,
+    // Links displayed in the header on the right side
+    externalLinks: [{
+        name: `Твиттер`,
+        url: `https://twitter.com/lekoarts_de`,
+      },
+      // {
+      //   name: `Instagram`,
+      //   url: `https://www.instagram.com/lekoarts.de/`,
+      // },
+    ],
+    // Navigation links
+    navigation: [{
+      title: `Записи`,
+      slug: `/blog`,
+    }, ],
   },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      // See the theme's README for all available options
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
-          },
-        ],
-      },
+  plugins: [{
+    resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+    options: {
+      showLineNumbers: false,
+      navigation: [{
+          title: `Записи`,
+          slug: `/blog`,
+        },
+        {
+          title: `Про меня`,
+          slug: `/about-me`,
+        },
+      ],
+      externalLinks: [{
+          name: `Твиттер`,
+          url: `https://twitter.com/lekoarts_de`,
+        },
+        // {
+        //   name: `Instagram`,
+        //   url: `https://www.instagram.com/lekoarts.de/`,
+        // },
+      ],
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
-}
+  }, ],
+};
---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/tags.tsx
@@ -0,0 +1,42 @@
+/** @jsx jsx */
+import { jsx, Link as TLink, Heading } from "theme-ui"
+import { Box, Flex } from "@theme-ui/components"
+import kebabCase from "lodash.kebabcase"
+import { Link } from "gatsby"
+import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
+import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
+import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes"
+
+type PostsProps = {
+    list: {
+        fieldValue: string
+        totalCount: number
+    }[]
+}
+
+const Tags = ({ list }: PostsProps) => {
+    const { tagsPath, basePath } = useMinimalBlogConfig()
+
+    return (
+        <Layout>
+            <SEO title="Tэги" />
+            <Heading variant="styles.h2">Тэги</Heading>
+            <Box mt={[4, 5]}>
+                {list.map((listItem) => (
+                    <Flex key={listItem.fieldValue} mb={[1, 1, 2]} sx={{ alignItems: `center` }}>
+                        <TLink
+                            as={Link}
+                            sx={{ variant: `links.listItem`, mr: 2 }}
+                            to={replaceSlashes(`/${basePath}/${tagsPath}/${kebabCase(listItem.fieldValue)}`)}
+                        >
+                            {listItem.fieldValue} <span sx={{ color: `secondary` }}>({listItem.totalCount})</span>
+                        </TLink>
+                    </Flex>
+                ))}
+            </Box>
+        </Layout>
+    )
+}
+
+export default Tags
