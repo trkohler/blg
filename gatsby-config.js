6b59ab8caa27ce60c0f2027d472e53cc9ee8a485
t.kohler <t.kohler@smartweb.com.ua>
2021-06-28 09:52:44 +0300

    purge css

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,20 @@ module.exports = {
     showCopyButton: true,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        purgeCSSOptions: {
+          // https://purgecss.com/configuration.html#options
+          // safelist: ['safelist'], // Don't remove this selector
+        },
+      },
+    },
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     {
