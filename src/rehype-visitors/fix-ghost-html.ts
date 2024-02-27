d245642dd16c6da92b00d32b78c7af94422a1247
Troy Kohler <troy.kohler@zalando.de>
2024-02-27 11:58:35 +0100

    fix styling for embedded ghost videos

--- src/rehype-visitors/fix-ghost-html.ts
+++ src/rehype-visitors/fix-ghost-html.ts
@@ -1,23 +1,34 @@
-import { visit } from 'unist-util-visit';
-import { LanguageUnion } from '../translations/langStrings';
-import { appendPostTrackingUtms } from '../utm_tracking';
+import { visit, Visited } from "unist-util-visit";
+import { LanguageUnion } from "../translations/langStrings";
+import { appendPostTrackingUtms } from "../utm_tracking";
 
-export const visit_and_fix_ghost_html = (options: { title: string, language: LanguageUnion }) => {
+export const visit_and_fix_ghost_html = (options: {
+  title: string;
+  language: LanguageUnion;
+}) => {
   return (tree) => {
-    visit(tree, 'element', visitor);
+    visit(tree, "element", visitor);
   };
 
   function visitor(node) {
-    if (node.tagName.startsWith('h')) {
+    if (node.tagName.startsWith("h")) {
       node.properties.className = [`cms-heading-${node.tagName}`];
-    } else if (node.tagName === 'a') {
-      
-      node.properties.href = appendPostTrackingUtms(options.title, options.language, node.properties.href)
+    } else if (node.tagName === "a") {
+      node.properties.href = appendPostTrackingUtms(
+        options.title,
+        options.language,
+        node.properties.href
+      );
       node.properties = {
         ...node.properties,
-        target: '_blank',
-        rel: 'noopener noreferrer',
+        target: "_blank",
+        rel: "noopener noreferrer",
       };
+    } else if (
+      node.tagName === "figure" &&
+      node.children[0].tagName === "iframe"
+    ) {
+      node.properties.className = ["youtube-video-container"];
     }
   }
 };
