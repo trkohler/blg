6e9e4eeee13b6ee86af5a91f8205fa04742fa0a5
T <troyalekson@gmail.com>
2024-03-03 12:02:04 +0100

    change post design (#11)

--- src/pages_components/post.tsx
+++ src/pages_components/post.tsx
@@ -7,27 +7,27 @@ import {
   Icon,
   useColorModeValue,
   Stack,
-} from '@chakra-ui/react';
-import React from 'react';
-import { unified } from 'unified';
-import rehypeParse from 'rehype-parse';
-import { Layout } from '../components/Layout';
-import rehypePrism from 'rehype-prism-plus'
-import stringify from 'rehype-stringify';
-import { Link } from '../components/Link';
-import { visit_highlight_code } from '../rehype-visitors/highlight-code';
-import NewsletterBoxCondenced from '../components/NewsletterBoxCondenced';
-import RelatedPosts from '../components/RelatedPosts';
-import { BsDot } from 'react-icons/bs';
-import { langStrings } from '../translations/langStrings';
-import { getLanguage } from '../translations/pathLangUtils';
-import { constructPath } from '../translations/constructCommonPath';
-import { useSiteMetadata } from '../hooks/use-site-medatadata';
-import { visit_and_fix_ghost_html } from '../rehype-visitors/fix-ghost-html';
-import { GhostHtmlPost } from '../components/GhostHtmlPost';
-import { OgType, Seo } from '../components/Seo';
-import { Comments } from '../components/Comments';
-import { AuthorBox } from '../components/AuthorBox';
+} from "@chakra-ui/react";
+import React from "react";
+import { unified } from "unified";
+import rehypeParse from "rehype-parse";
+import { Layout } from "../components/Layout";
+import rehypePrism from "rehype-prism-plus";
+import stringify from "rehype-stringify";
+import { Link } from "../components/Link";
+import { visit_highlight_code } from "../rehype-visitors/highlight-code";
+import NewsletterBoxCondenced from "../components/NewsletterBoxCondenced";
+import RelatedPosts from "../components/RelatedPosts";
+import { BsDot } from "react-icons/bs";
+import { langStrings } from "../translations/langStrings";
+import { getLanguage } from "../translations/pathLangUtils";
+import { constructPath } from "../translations/constructCommonPath";
+import { useSiteMetadata } from "../hooks/use-site-medatadata";
+import { visit_and_fix_ghost_html } from "../rehype-visitors/fix-ghost-html";
+import { GhostHtmlPost } from "../components/GhostHtmlPost";
+import { OgType, Seo } from "../components/Seo";
+import { Comments } from "../components/Comments";
+import { AuthorBox } from "../components/AuthorBox";
 
 type PostPageProps = {
   data: {
@@ -94,12 +94,11 @@ const Post = ({
     .use(stringify)
     .processSync(post.html)
     .toString();
-  
 
   const correctTags = post.tags.filter((tag) => tag.visibility !== `internal`);
   const lastTag = correctTags[correctTags.length - 1];
   const correctTagsWithoutLast = correctTags.slice(0, -1);
-  const accentGray = useColorModeValue('gray.500', 'gray.400');
+  const accentGray = useColorModeValue("gray.500", "gray.400");
 
   return (
     <>
@@ -116,32 +115,35 @@ const Post = ({
         location={pathname}
       >
         <Stack
-          alignItems={'start'}
+          alignItems={"start"}
           mt={[6, 32]}
           mx={4}
           spacing={10}
-          direction={['column', 'row']}
+          direction={["column", "row"]}
         >
           <VStack
             spacing={12}
-            align={'stretch'}
-            pr={[0, 0, 10]}
-            maxW={'4xl'}
+            align={"stretch"}
+            maxW={["lg", "lg", "3xl", "5xl", "6xl"]}
             // borderWidth={'1px'}
             // borderColor={'gray.200'}
           >
             <Box textAlign="center">
-              <Heading size={['lg', '2xl']}>{post.title}</Heading>
+              <Heading size={["lg", "2xl"]}>{post.title}</Heading>
             </Box>
 
             <HStack
-              spacing={'8'}
-              justifyContent={'center'}
-              fontSize={['xs', 'xs', 'md']}
+              spacing={"8"}
+              justifyContent={"center"}
+              fontSize={["xs", "xs", "md"]}
             >
               <Text>{post.published_at}</Text>
               <Text>{post.reading_time} min read</Text>
-              <HStack spacing={'0'} fontWeight={'bold'} color={accentGray}>
+              <HStack
+                spacing={"0"}
+                fontWeight={"bold"}
+                color={accentGray}
+              >
                 {correctTagsWithoutLast.map((tag) => {
                   return (
                     <>
@@ -164,16 +166,21 @@ const Post = ({
               </HStack>
             </HStack>
 
-            <Box color={'gray.400'} textAlign="center">
+            <Box
+              color={"gray.400"}
+              textAlign="center"
+            >
               <Text>
                 {langStrings.last_time_updated[language]} {post.updated_at}
               </Text>
             </Box>
             <GhostHtmlPost content={content} />
           </VStack>
-          <AuthorBox language={language} />
         </Stack>
-        <VStack py={24} spacing={16}>
+        <VStack
+          py={24}
+          spacing={16}
+        >
           <NewsletterBoxCondenced language={language} />
           {/* 
             because of query logic relatedPost would always 
@@ -185,7 +192,6 @@ const Post = ({
               relatedPosts={relatedPosts}
               language={language}
             />
-            
           )}
           <Comments />
         </VStack>
